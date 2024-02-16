49a573373425381f036a4a866eed5d7fd6bb000b
Fabien Cazenave <fabien@cazenave.cc>
2024-02-16 17:14:24 +0100

    licenses

--- js/dactylo.js
+++ js/dactylo.js
@@ -1,3 +1,23 @@
+/**
+ * WebTypist v3 (preview) a.k.a. DuckTypist.
+ * https://github.com/OneDeadKey/webtypist
+ *
+ * Copyleft (C) 2010-2024, Fabien & Léo Cazenave.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const QUACK = new Audio('quack.mp3');
 const MIN_PRECISION  = 98;  // percentage of correct keys
 const MIN_CPM_SPEED  = 100; // characters per minute
