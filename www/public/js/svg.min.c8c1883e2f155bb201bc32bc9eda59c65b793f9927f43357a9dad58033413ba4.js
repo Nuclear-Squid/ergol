f7ca0b97ad65ab68e2eb290b1b719a62017cb4c4
Nuclear Squid <leo@cazenave.cc>
2024-02-28 13:45:04 +0100

    CI pour builder le site web avec hugo.

--- www/public/js/svg.min.c8c1883e2f155bb201bc32bc9eda59c65b793f9927f43357a9dad58033413ba4.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded",()=>{"use strict";const e=document.querySelector(".keyboard img"),t=document.querySelector(".keyboard select");fetch(e.getAttribute("src")).then(e=>e.text()).then(n=>{e.outerHTML=n,t.innerHTML=`
-        <option value="dk iso intlBackslash am">       ISO-A </option>
-        <option value="dk iso intlBackslash" selected> ISO   </option>
-        <option value="dk ansi">                       ANSI  </option>
-        <option value="dk ol60 ergo">                  TMx   </option>
-        <option value="dk ol50 ergo">                  4×6   </option>
-        <option value="dk ol40 ergo">                  3×6   </option>
-      `;const s=document.querySelector(".keyboard svg");t.addEventListener("change",e=>{s.setAttribute("class",e.target.value)})})})
